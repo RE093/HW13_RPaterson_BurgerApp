54047556776c75834f37fd3f1dd1dceadeb191db
RE000 <re.paterson000@gmail.com>
2020-08-08 16:45:39 +1000

    push to heroku

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,17 +1,24 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE
-});
+let connection;
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+  });
+}
 
 module.exports = connection;
